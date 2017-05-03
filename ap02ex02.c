a21d20bf76b4ce384a2e2e37f41711d1b9aa2f36
Andregarciav <andregarcia@ufmg.br>
2017-05-03 18:30:12 -0300

    Update ap02ex02.c

--- ap02ex02.c
+++ ap02ex02.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    float d, p, s, t;
-    d = 500;
-    d = (d + 0.01*d) + 0.01*(d + 0.01*d) + 0.01*((d + 0.01*d) + 0.01*(d + 0.01*d));
+    float d, p, s, t; // declaração de variaveis
+    d = 500; //define o valor de d, poderia ser definido junto da declaração de variaveis
+    d = (d + 0.01*d) + 0.01*(d + 0.01*d) + 0.01*((d + 0.01*d) + 0.01*(d + 0.01*d)); //
 
     /*p = d + 0.01*d;
     s = p + 0.01*p;
     t = s + 0.01*s;*/
-    printf("Valor na conta = %.2f\n" , d);
+    printf("Valor na conta = %.2f\n" , d); //imprime o valor na conta após 3 meses
     return 0;
 }
