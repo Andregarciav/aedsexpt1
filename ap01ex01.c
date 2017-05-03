2dda214647129badf8b2ee0cd980abb429b7d801
Andregarciav <andregarcia@ufmg.br>
2017-05-03 18:18:20 -0300

    Update ap01ex01.c

--- ap01ex01.c
+++ ap01ex01.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define PI 3.14159265
+#define PI 3.14159265 //define o valor de pi
 int main()
 {
-    double angulo, resultado;
-    printf("Escreva seu angulo em graus:\n");
-    scanf("%lf", &angulo);
-    resultado = sin (angulo * PI / 180);
-    printf("O seno do angulo %lf eh: %lf \n", angulo ,resultado);
-    return 0;
+    double angulo, resultado; //declação de variáveis tipo lf
+    printf("Escreva seu angulo em graus:\n"); //imprime na tela
+    scanf("%lf", &angulo); //le o valor do angulo e armazena
+    resultado = sin (angulo * PI / 180); // operação matemática para calculo do seno 
+    printf("O seno do angulo %lf eh: %lf \n", angulo ,resultado); // imprime o valor do angulo e seu seno
+    return 0; // se tudo der certo, retorna 0
 }
