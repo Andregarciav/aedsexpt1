3addce3237b403753a00b538d755134587a3ceca
Andregarciav <andregarcia@ufmg.br>
2017-05-04 16:39:04 -0300

    Update ap03ex3.c

--- ap03ex3.c
+++ ap03ex3.c
@@ -8,25 +8,25 @@ int main()
     int num1, num2;
     int aux;
     printf("Escreva o primeiro numero: \n");
-    scanf(" %d" , &num1);
-    printf(" Escreva o segundo numero: \n");
-    scanf(" %d" , &num2);
-    if (num1 > num2) // rearanja o maior
+    scanf("%d" , &num1);
+    printf("Escreva o segundo numero: \n");
+    scanf("%d" , &num2);
+    if (num1 > num2) // rearranja o maior
     {
         aux = num1;
         num1 = num2;
         num2 = aux;
     }
-    printf("%d %d" , num1, num2);
-    if ( ( num1 % 2 ) == 0) //verifica se par ou impar
+    if ( ( num1 % 2 ) == 0) //verifica se par ou impar, se par incrementa 
     {
         num1+=1;
-        printf(" %d ; ", num1);
+        printf(" %d ;", num1); //imprime a incrementação
     }
-    while (num1 < num2);
+    while (num1 < (num2-2)) //estrutura de repetição
     {
         num1 += 2;
-        printf(" %d ; ", num1);
+        printf(" %d ;", num1); //imprime todos os impares do intervalo
     }
+    printf("\n");
     return 0;
 }
