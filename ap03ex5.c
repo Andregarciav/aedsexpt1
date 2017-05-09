a9ab7519a7a4b6dfabe579e1421d3f40750475c5
Andregarciav <andregarcia@ufmg.br>
2017-05-09 17:10:38 -0300

    Update ap03ex5.c

--- ap03ex5.c
+++ ap03ex5.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void calcula_salario (double alimentacao, double transporte, double metragem)
+void calcula_salario (double alimentacao, double transporte, double metragem) //define as funções flutuantes
 {
-  double salario;
+  double salario; //define a função salário
   salario  = alimentacao +  (2*transporte);
-  if (metragem < 10)
+  if (metragem < 10) //condição para valor a ser pago pela produtividade
     {
-      metragem *= 10;
-      salario = metragem + salario;
-      printf("O pedreiro deve receber R$ %.2lf\n", salario);
+      metragem *= 10; //valor da produção por metro2
+      salario = metragem + salario; //calcula o valor a ser recebido
+      printf("O pedreiro deve receber R$ %.2lf\n", salario); //imprime valor a ser recebido
     }
-  else if (metragem >= 10 && metragem < 20 )
+  else if (metragem >= 10 && metragem < 20 ) //condição para valor a ser pago pela produtividade
     {
-      metragem *= 11.5;
-      salario = metragem + salario;
-      printf("O pedreiro deve receber R$ %.2lf\n", salario);
+      metragem *= 11.5; //valor da produção por metro2
+      salario = metragem + salario; //calcula valor a ser recebido
+      printf("O pedreiro deve receber R$ %.2lf\n", salario); //imprime valor a ser recebido
     }
-  else if (metragem > 20)
+  else if (metragem > 20) //condição para valor a ser pago pela produtividade
     {
-      metragem *= 13 ;
-      salario = metragem + salario;
-      printf("O pedreiro deve receber R$ %.2lf\n", salario);
+      metragem *= 13 ; //valor da produção por metro2
+      salario = metragem + salario; //calcula valor a ser recebido 
+      printf("O pedreiro deve receber R$ %.2lf\n", salario); //imprime valor a ser recebido
     }
 }
 
 int main()
 { double a, t, m;
    printf("Calculadora de salário\nDigite o valor do vale alimentacao:\n");
-   scanf("%lf", &a);
+   scanf("%lf", &a); //lê vapor inserido e armazena em a
    printf("Digite o valor do vale transporte:\n");
-   scanf("%lf", &t);
+   scanf("%lf", &t); //lê o vapor inserido e armazena em t
    printf("Digite a quantidade produzida:\n");
-   scanf("%lf", &m);
-   calcula_salario ( a , t , m ); 
-   return 0;
+   scanf("%lf", &m); //le o valor inserido e armazena em m
+   calcula_salario ( a , t , m ); //realiza o cálculo de acordo com as variaveis pré definidas usando os dados inseridos pelo usuário
+   return 0; //retorna 0 se tudo der certo
 }
