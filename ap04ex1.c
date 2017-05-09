2265813594cef4f930ed2ae228eeaeb24883c14e
Andregarciav <andregarcia@ufmg.br>
2017-05-09 17:42:02 -0300

    Update ap04ex1.c

--- ap04ex1.c
+++ ap04ex1.c
@@ -5,17 +5,17 @@
 int main()
 {
   float vetorFloat[7];
-  int i;
-  for( i = 0 ; i < 7 ; i++)
+  int i; //definição da função inteira i
+  for( i = 0 ; i < 7 ; i++) //condições para função i
     {
-    printf("Digite o %do numero:\n", i+1);
-    scanf("%f" , &vetorFloat[i] );
+    printf("Digite o %do numero:\n", i+1); //imprime comando
+    scanf("%f" , &vetorFloat[i] ); //lê e armazena
     }
-  printf("Os numeros digitados sao:\n");
-  for(i = 2 ; i < 7 ; i += 2)
+  printf("Os numeros digitados sao:\n"); //imprime os números digitados
+  for(i = 2 ; i < 7 ; i += 2) //condições para a função i
     {
-      printf("%.0f ", vetorFloat[i]);
+      printf("%.0f ", vetorFloat[i]); //lê e armazena
     }
-  printf("\n");
-  return 0;
+  printf("\n"); //imprime resultado
+  return 0; //retorna 0 se dee certo
 }
